773ca9f1eda7f4145c10bec991221416a28a9117
williamjohngardner <bill@williamjohngardner.com>
2016-08-13 14:29:40 -0400

    Updating Static Root

--- billslist/settings.py
+++ billslist/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
 REST_FRAMEWORK = {
